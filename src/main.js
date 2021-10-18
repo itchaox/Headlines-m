131d53ce99f00f81789bd5a1b73bf6e333e6882d
itchaox <1462831573@qq.com>
2021-10-18 20:17:55 +0800

    '引入icon;导入Vant'

--- src/main.js
+++ src/main.js
@@ -2,9 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+// 加载 Vant 核心组件库
+import Vant from 'vant'
+// 加载 Vant 全局样式
+import 'vant/lib/index.css'
 // 加载全局样式
 import './styles/index.less'
+
+// 注册使用 Vant 组件库
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
